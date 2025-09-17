f9d71e628e226e099fe3f41d0b8b059aa9051edb
Naygara Sampaio <82398123+Soaresns@users.noreply.github.com>
2025-09-17 17:09:39 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 const clientesRoutes = require('./routes/clientesRoutes');
 const vendedoresRoutes = require('./routes/vendedoresRoutes');
+const categoriasRoutes = require('./routes/categoriasRoutes');
 
 app.use(express.json());
 
 app.use('/clientes', clientesRoutes);
 app.use('/vendedores', vendedoresRoutes);
+app.use('/categorias', categoriasRoutes);
 
 
 const PORT = process.env.PORT
